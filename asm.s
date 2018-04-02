bb4f09ec878df8bee54fe2654c7dca5f7b1da4a3
sharondaniely <35177861+sharondaniely@users.noreply.github.com>
2018-04-02 13:35:30 +0300

    created the function

--- asm.s
+++ asm.s
@@ -1,35 +1,71 @@
 section .data                    	; data section, read-write
-        an:    DQ 0              	; this is a temporary var
+        result:    DQ 0              	; this is a temporary var
 
 section .text                    	; our code is always in the .text section
-        global add_func          	    ; makes the function appear in global scope
-        extern printf            	; tell linker that printf is defined elsewhere 							; (not used in the program)
-
-add_func:                        	    ; functions are defined as labels
-        push    rbp              	; save Base Pointer (bp) original value
+        global add_func  			; makes the function appear in global scope
+		global sub_func
+		global mul_func
+		global div_func
+		
+		
+		
+add_func:							; functions are defined as labels
+        mov rax,0					; init rax to zero
+		push    rbp              	; save Base Pointer (bp) original value
         mov     rbp, rsp         	; use base pointer to access stack contents
-        mov rcx, rdi			    ; get function argument
-
-;;;;;;;;;;;;;;;; FUNCTION EFFECTIVE CODE STARTS HERE ;;;;;;;;;;;;;;;; 
-
-	mov	qword [an], 0		; initialize answer
-	label_here:
-                
-            
-                
-                
-   		end:
-
-		inc rcx      	    ; increment pointer
-		cmp byte [rcx], 0   ; check if byte pointed to is zero
-		jnz label_here      ; keep looping until it is null terminated
+		
+		add rdi, rsi
+		add rdi, rdx
+		
 
 
-;;;;;;;;;;;;;;;; FUNCTION EFFECTIVE CODE ENDS HERE ;;;;;;;;;;;;;;;; 
-
-         mov     rax,[an]         ; return an (returned values are in rax)
-         mov     rsp, rbp
-         pop     rbp
-         ret 
+        mov     rax,rdi		; return an (returned values are in rax)
+        mov     rsp, rbp
+        pop     rbp
+        ret 
 		 
-   
+ sub_func:							; functions are defined as labels
+        mov rax,0					; init rax to zero
+		push    rbp              	; save Base Pointer (bp) original value
+        mov     rbp, rsp         	; use base pointer to access stack contents
+		
+		sub rdi, rsi
+		sub rdi, rdx
+		
+
+
+        mov     rax,rdi		; return an (returned values are in rax)
+        mov     rsp, rbp
+        pop     rbp
+        ret 
+
+		
+		
+mul_func:							; functions are defined as labels
+        mov rax,0					; init rax to zero
+		push    rbp              	; save Base Pointer (bp) original value
+        mov     rbp, rsp         	; use base pointer to access stack contents
+		
+		mul rdi, rsi
+		
+        mov     rax,rdi		; return an (returned values are in rax)
+        mov     rsp, rbp
+        pop     rbp
+        ret 
+
+		
+		
+				
+div_func:							; functions are defined as labels
+        mov rax,0					; init rax to zero
+		push    rbp              	; save Base Pointer (bp) original value
+        mov     rbp, rsp         	; use base pointer to access stack contents
+		
+		div rdi, rsi
+		
+        mov     rax,rdi		; return an (returned values are in rax)
+        mov     rsp, rbp
+        pop     rbp
+        ret 
+
+		
